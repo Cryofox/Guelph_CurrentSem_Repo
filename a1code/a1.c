e5606da06f617f80b36cd52589547660656f9ad8
Cryofox <hstances@uoguelph.ca>
2015-01-21 22:43:42 -0800

    Finished, Clouds move and wrap.
    All that is left is to Test on lab Macs, Split code into Files i.e.
    Cloud code -> Cloud.C , cloud.H aswell.  Perlin.c and perlin.h
    Gravity can be left in a1. Then Comment and Submiterino.

--- a1code/a1.c
+++ a1code/a1.c
@@ -105,7 +105,7 @@ extern void tree(float, float, float, float, float, float, int);
    //List of Cloud_Particles     
       Cloud_Particle* cloud_voxels;
    //Particle Length of Array
-      int* cloudLength;
+      int cloudLength;
    }Cloud;
 
 
@@ -116,9 +116,9 @@ extern void tree(float, float, float, float, float, float, int);
    void DestroySkyClouds();
 
    //This Creates a Single Cloud
-   Cloud CreateCloud();
+   Cloud* CreateCloud();
    //This Destroys a Single Cloud
-   void DestroyCloud(Cloud cloud);
+   void DestroyCloud(Cloud* cloud);
 
 
    float ComputePerlin_Value(Gradient_Table gTable, float x, float y);
@@ -129,7 +129,7 @@ extern void tree(float, float, float, float, float, float, int);
 
 
 //----Global Values-----
-Cloud* skyClouds;
+Cloud** skyClouds;
 
 
    //----CONTROLS-----
@@ -143,22 +143,16 @@ Cloud* skyClouds;
    const int SIZE=256;
 
    //The number of Clouds you'd like to populate the world
-   int num_Clouds=5;
-
+   int num_Clouds=30;
+   int max_CloudSize=30;
    //These Values are used to manipulate the direction wind is applied in
    //used for Cloud Movement
+   //Diagonal movements may cause jittering due to centroid + mod
    float windForce_X=0;
-   float windForce_Z=1;
+   float windForce_Z=0.1;
    //=================
 
 //=================
-
-
-
-
-
-
-
 
 	/*** collisionResponse() ***/
 	/* -performs collision detection and response */
@@ -178,14 +172,11 @@ void collisionResponse()
    float currLoc_Y;
    float currLoc_Z;
 
-
    getViewPosition(&currLoc_X, &currLoc_Y, &currLoc_Z);
-
 
    currLoc_X*=-1;
    currLoc_Y*=-1;
    currLoc_Z*=-1;
-
    currLoc_Y-=.5;
    //If it's a green block, don't move?
 
@@ -193,7 +184,7 @@ void collisionResponse()
    if(world[(int)currLoc_X][(int)currLoc_Y][(int)currLoc_Z]!=0 && world[(int)currLoc_X][(int)currLoc_Y][(int)currLoc_Z]!=2)
    {
       //Silly Statement the computer says. Wouldn't you scream if you walked through walls?
-      printf("IM ENTERING A BLOCK AHAHHHHHHH=\n");
+      //printf("IM ENTERING A BLOCK AHAHHHHHHH=\n");
 
       //Reuse xyz variables, as condition has been checked
       //This is useless if keyboard was not pressed
@@ -229,14 +220,8 @@ void collisionResponse()
  
          if(world[(int)currLoc_X][ (int)(oldLoc_Y)][(int)currLoc_Z]!=0) 
          {      
-
-            //oldLoc_Y=oldLoc_Y; 
             if(world[(int)currLoc_X][ (int)(oldLoc_Y+1)][(int)currLoc_Z]==0)
             {
-            printf("No Moving down! \n");
-
-            //If we are not moving 
-            //if(world[(int)currLoc_X][(int)(oldLoc_Y)][(int)currLoc_Z])
             oldLoc_X=currLoc_X;
             oldLoc_Z=currLoc_Z; 
             oldLoc_Y+=1;
@@ -249,11 +234,8 @@ void collisionResponse()
          }
 
       setViewPosition((oldLoc_X*-1), (oldLoc_Y*-1), (oldLoc_Z*-1));
-
-      printf("My Old Location:%f,%f,%f\n",oldLoc_X,oldLoc_Y,oldLoc_Z);  
-    //  setViewPosition(currLoc_X, currLoc_Y-1.6, currLoc_Z);
    }
-   printf("My View Location:%f,%f,%f\n",currLoc_X,currLoc_Y,currLoc_Z);  
+ 
 
 }
 
@@ -319,7 +301,7 @@ float *la;
 
 	/* your code goes here */
    //Gravity Code
-   //ApplyGravity();
+   ApplyGravity();
    UpdateCloudMovement();
    }
 
@@ -328,114 +310,111 @@ float *la;
    
    void CreateSkyClouds()
    {
-      skyClouds = malloc(num_Clouds * (sizeof(Cloud*)));
+      skyClouds = malloc(num_Clouds * (sizeof(Cloud)));
       for(int i=0;i<num_Clouds;i++)
       {
-         printf("Creating Cloud:%d\n",i);
          skyClouds[i]=CreateCloud();
       }
    }
-   Cloud CreateCloud()
+   Cloud* CreateCloud()
    {
-      Cloud cloud;// = malloc(sizeof(Cloud*));
+      Cloud* cloud = malloc(sizeof(Cloud));
 
       //Initialize Centroid Location
-      //cloud.pos_X= malloc(sizeof(float));    
-      //cloud.pos_Y= malloc(sizeof(float));    
-      //cloud.pos_Z= malloc(sizeof(float));    
 
       //Randomize the Cloud Start Position
-      cloud.pos_Y=50; //Set Heigh to Max Height
+      cloud->pos_Y=50; //Set Heigh to Max Height
 
-      cloud.pos_X= (int)(rand())% WORLDX;
-      cloud.pos_Z= (int)(rand())% WORLDZ;  
+      cloud->pos_X= (int)(rand())% WORLDX;
+      cloud->pos_Z= (int)(rand())% WORLDZ;  
 
       //Create Cloud Shape
-      cloud.cloudLength=malloc(sizeof(int));
 
-      int cloudSize=5;
-      cloud.cloudLength=cloudSize; //Cloud is made up of 5 Voxels
+
+      //How many voxels a cloud can be made up of at most
+      cloud->cloudLength=max_CloudSize; 
 
       //Use CloudSize due to C complaining about int*.
-      cloud.cloud_voxels = malloc( cloudSize * (sizeof(Cloud_Particle)));
-  
+      cloud->cloud_voxels = malloc( cloud->cloudLength * (sizeof(Cloud_Particle))); 
     //First Value must be Center
-      cloud.cloud_voxels[0]= CreateParticle(0, 0, 0);
+      cloud->cloud_voxels[0]= CreateParticle(0, 0, 0);
 
+      int cloudCount=1;
       //Loop Through List 
-      for(int i=1;i<cloud.cloudLength;i++)
+      for(int i=1;i<cloud->cloudLength;i++)
       {
+         int wasCloudAdded=0; //1= True
          //Increment through already made List to create neighbours in it
          for(int j=0;j<i;j++)
          {
             //For each Point in the list add a Neighbour
-            int xPos= cloud.cloud_voxels[j].pos_X;
-            int zPos= cloud.cloud_voxels[j].pos_Z;
+            int xPos= cloud->cloud_voxels[j].pos_X;
+            int zPos= cloud->cloud_voxels[j].pos_Z;
             
             int xOffset;
             int zOffset;
 
 
+
             //Right
             xOffset=1+ xPos;
             zOffset=0+ zPos;
-            if(isDuplicate(cloud, xOffset, zOffset)==0)
-            {
-               cloud.cloud_voxels[i]= CreateParticle(xOffset,0,zOffset);
-               break;
-            }
+            //Random will be used to decide whether a vox is added or not
+            if( (rand()%100) >50)
+               if(isDuplicate(cloud, xOffset, zOffset)==0)
+               {
+                  cloudCount++;
+                  cloud->cloud_voxels[i]= CreateParticle(xOffset,0,zOffset);
+                  wasCloudAdded=1;
+                  break;
+               }
 
             //Left
             xOffset=-1+ xPos;
             zOffset=0+ zPos;
-            if(isDuplicate(cloud, xOffset, zOffset)==0)
-            {
-               cloud.cloud_voxels[i]= CreateParticle(xOffset,0,zOffset);
-               break;
-            }
+            if( (rand()%100) >50)
+               if(isDuplicate(cloud, xOffset, zOffset)==0)
+               {
+                  cloudCount++;
+                  cloud->cloud_voxels[i]= CreateParticle(xOffset,0,zOffset);
+                  wasCloudAdded=1;
+                  break;
+               }
             //Forward
             xOffset=0+ xPos;
             zOffset=1+ zPos;
-            if(isDuplicate(cloud, xOffset, zOffset)==0)
-            {
-               cloud.cloud_voxels[i]= CreateParticle(xOffset,0,zOffset);
-               break;
-            }
+            if( (rand()%100) >50)
+               if(isDuplicate(cloud, xOffset, zOffset)==0)
+               {
+                  cloudCount++;
+                  cloud->cloud_voxels[i]= CreateParticle(xOffset,0,zOffset);
+                  wasCloudAdded=1;
+                  break;
+               }
             //Backward
             xOffset=0+ xPos;
             zOffset=-1+ zPos;
-            if(isDuplicate(cloud, xOffset, zOffset)==0)
-            {
-               cloud.cloud_voxels[i]= CreateParticle(xOffset,0,zOffset);
-               break;
-            }
+            if( (rand()%100) >50)            
+               if(isDuplicate(cloud, xOffset, zOffset)==0)
+               {
+                  cloudCount++;
+                  cloud->cloud_voxels[i]= CreateParticle(xOffset,0,zOffset);
+                  wasCloudAdded=1;
+                  break;
+               }
+
+
          }
+         //Cloud was not added, realign I
+         if(wasCloudAdded==0)
+            i--;
       }
       return cloud;
    }
 
-/*
-   //This is 1 "Voxel"/Cube that Makes up the Cloud
-   typedef struct{
-      int* pos_X;
-      int* pos_Y;
-      int* pos_Z;
-   }Cloud_Particle;
-
-   typedef struct{
-   //This is the Centroid position of the Cloud.
-   //All of the Cloud Particles will be Offset based on this
-   //center position
-      float* pos_X;
-      float* pos_Y;
-      float* pos_Z;      
-      Cloud_Particle* cloud_voxels;
-      int* cloudLength;
-   }Cloud;
-*/
-
 
    //0=False, 1 = True
+   //Checks if a cloudVoxel exists at the offset location provided
    int isDuplicate(Cloud cloud, int xOffset, int zOffset)
    {
       for(int i=0;i<cloud.cloudLength;i++)
@@ -449,7 +428,7 @@ float *la;
    }
 
 
-
+   //Creates a Cloud Particle
    Cloud_Particle CreateParticle(int x, int y, int z)
    {
       Cloud_Particle cP;// = malloc(sizeof(Cloud_Particle));
@@ -465,47 +444,106 @@ float *la;
    }
 
 
-
+   //Animates the Clouds
    void UpdateCloudMovement()
    {
+      //Clear the Sky of clouds
+      for(int x=0;x<WORLDX;x++)
+         for(int z=0;z<WORLDZ;z++)
+            if(world[x][WORLDY-1][z]==5)
+               world[x][WORLDY-1][z]=0;
+
+      //Draw Clouds   
+      //Increment Cloud Position
+      for(int i=0;i<num_Clouds;i++)
+      {
+         skyClouds[i]->pos_X += windForce_X;
+         skyClouds[i]->pos_Z += windForce_Z;
+
+      //The Actual cloud position does not matter due to all values being modded.
+      //Modding the offsets off the centroid equates to clouds which wrap around the sky
+      //this is a very cool effect which is why I'm doing it this way.
+
+         //if skyClouds[i] >200 (twice the size of the box, then reset to 0)
+         if(skyClouds[i]->pos_X> WORLDX*2)
+            skyClouds[i]->pos_X=0;
+          if(skyClouds[i]->pos_Z> WORLDZ*2)
+            skyClouds[i]->pos_Z=0;        
+
+         //For Each Cloud Create White Cubes at correct Location
+         //printf("Cloud[%d]: Length:%d", i, skyClouds[i]->cloudLength);
+         for(int j=0;j<skyClouds[i]->cloudLength;j++)
+         {
+            //Find Location
+            int cloudCube_X = (int)skyClouds[i]->pos_X;
+            int cloudCube_Z = (int)skyClouds[i]->pos_Z;
+
+            //Apply offset
+            cloudCube_X+= skyClouds[i]->cloud_voxels[j].pos_X;
+            cloudCube_Z+= skyClouds[i]->cloud_voxels[j].pos_Z;
+
+
+            //Mod for World Wrap
+            if(cloudCube_X>=0)
+               cloudCube_X= cloudCube_X% WORLDX;     
+            //This has to be done due to -Numbers not modding Correctly
+            else      
+               {
+                  cloudCube_X = WORLDX-((-1*cloudCube_X) % WORLDX);
+               }
+
+            if(cloudCube_Z>=0)
+               cloudCube_Z= cloudCube_Z% WORLDZ;     
+            //This has to be done due to -Numbers not modding Correctly
+            else      
+               {
+                  cloudCube_Z = WORLDZ-((-1*cloudCube_Z) % WORLDZ);
+               }
+
+            //If the Cell is Empty
+            if( world[cloudCube_X][WORLDY-1][cloudCube_Z]==0)
+               world[cloudCube_X][WORLDY-1][cloudCube_Z]=5;
+         }
+      }
 
    }
 
 
+   //Free Malloced Memory
    void DestroySkyClouds()
    {
       for(int i=0;i<num_Clouds;i++)
       {
-         printf("Destroying a Cloud! [%d]\n",i);
+      //   printf("Destroying a Cloud! [%d]\n",i);
          DestroyCloud(skyClouds[i]);
       }
       free(skyClouds);
-      printf("Free this:Skycloud\n");
+      //printf("Free this:Skycloud\n");
    }
 
-   void DestroyCloud(Cloud cloud)
+   //Free Malloced Memory
+   void DestroyCloud(Cloud* cloud)
    {
-      printf("Free: Cloud XYZ\n");
+      //printf("Free: Cloud XYZ\n");
       //free(cloud.pos_X);
       //free(cloud.pos_Y);
       //free(cloud.pos_Z);
-      printf("Free:cloud_Voxels\n");
-      for(int i=0;i<cloud.cloudLength;i++)
+      //printf("Free:cloud_Voxels\n");
+      for(int i=0;i<cloud->cloudLength;i++)
       {
-         printf("Free: Cloud Vox[%d] XYZ\n",i);
+         //printf("Free: Cloud Vox[%d]/%d XYZ\n",i,cloud->cloudLength);
           //free(cloud.cloud_voxels[i].pos_X);
           //free(cloud.cloud_voxels[i].pos_Y);
           //free(cloud.cloud_voxels[i].pos_Z);        
       }
-      free(cloud.cloud_voxels);
-      printf("Free this:Cloud\n");
+      free(cloud->cloud_voxels);
+      free(cloud);
+      //printf("Free this:Cloud\n");
    }
 
 
 
-
-
-   //This function will decrement the value by -1.1 if a block is not directly underneath
+   //This function will decrement the value by .1 if a block is not directly underneath
    void ApplyGravity()
    {
       float gravity = .1;
@@ -523,18 +561,13 @@ float *la;
       currLoc_Y*=-1;
       currLoc_Z*=-1;  
 
-      //Take y Value Check if at minus gravity a block exists
+ 
 
-
-      //if(world[(int)currLoc_X][(int)currLoc_Y][(int)currLoc_Z] )
-
-
-      //Call Collision Response Manually, as it does not get Called
-      //unless provided keyboard input
+      //Call Collision Responds Manually, as it does not get Called
+      //unless provided keyboard input. Therefore fix via manual invoction
       //Collision Code:
       collisionResponse();
    }
-
 
 
 
@@ -642,16 +675,19 @@ int i, j, k;
       //Cleanup after self
 
       //
-      printf("ExitCleanupCode\n");
+      //printf("ExitCleanupCode\n");
       DestroySkyClouds();
    }
 
 
 
 
-//Perlin Noise Utilizes Gradient Vectors to Create Relational Data.
+//Perlin Noise Utilizes Gradient Vectors to Create Relational Data. THAT is TRUE perlin noise.
+//The way it works is by generating Vector "Gradients" at integer locations, then provided a 
+//a float, find out how far it is from its nearest surrounding 4 vectors, and then 
+//interpolate. The below code uses Bi-Cubic interpolation (a bit math heavy), and vectors
 
-
+   //Free Malloced Memory
    void DestroyGradientTable(Gradient_Table gT)
    {
       free(gT.permutationReference);
@@ -659,6 +695,8 @@ int i, j, k;
       free(gT.gradientY);
    }
 
+   //http://www.angelcode.com/dev/perlin/perlin.html
+   //The code in this function is referenced from here, I simply chose to store it in a struct
    //Create Permutation Table
    Gradient_Table CreateGradientTable(int seed)
    {
@@ -667,9 +705,6 @@ int i, j, k;
       gT.permutationReference = malloc(sizeof(int)*SIZE);
       gT.gradientX=malloc(sizeof(float)*SIZE);
       gT.gradientY=malloc(sizeof(float)*SIZE);
-
-      //Create the Pointer Array
-    //  float ptr_Array[256];
 
       //Initialize Array
       for(int i=0; i< SIZE;i++)
@@ -704,7 +739,6 @@ int i, j, k;
    //http://www.angelcode.com/dev/perlin/perlin.html
    //The code in this function is referenced from here. It helped me solve the math behind 
    //the math involved with perlin noise such as gradient dot product
-
    float ComputePerlin_Value(Gradient_Table gTable, float x, float y)
    {
       //Step 1: Find the Integer position of the 4 Surrounding Points
@@ -735,28 +769,18 @@ int i, j, k;
       //Step 4: Compute the dot-product between the vectors and the gradients
       float v00 = gTable.gradientX[q00]*tx0 + gTable.gradientY[q00]*ty0;
       float v01 = gTable.gradientX[q01]*tx1 + gTable.gradientY[q01]*ty0;
-
       float v10 = gTable.gradientX[q10]*tx0 + gTable.gradientY[q10]*ty1;
       float v11 = gTable.gradientX[q11]*tx1 + gTable.gradientY[q11]*ty1;
 
-      //printf("V00=%f ",v00);
-      //printf("V01=%f ",v01);
-      //printf("V10=%f ",v10);
-      //printf("V11=%f \n",v11);
-
       // Do the bi-cubic interpolation to get the final value
-      float wx = (3.0 - 2.0*tx0)*tx0*tx0;
-      //printf("wx=%f \t",wx);   
+      float wx = (3.0 - 2.0*tx0)*tx0*tx0; 
       float v0 = v00 - wx*(v00 - v01);
       float v1 = v10 - wx*(v10 - v11);
-      //printf("v1=%f \t",v1);
-
       float wy = (3.0 - 2.0*ty0)*ty0*ty0;
-      //printf("wy=%f \t",wy);
+
       //Final Value
       float v = v0 - wy*(v0 - v1);
 
-      //printf("fV=%f",v);
       return v;
    }
 
